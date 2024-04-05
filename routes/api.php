1fe7fea2a2f413186762c58de50f103d6859e873
Guivzz <guivazoliveira2002@gmail.com>
2024-04-05 11:26:27 -0300

    RESTful API with Laravel: Build a Real API with Laravel

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Buyer 
+Route::resource('buyers', 'App\Http\Controllers\Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+//Category
+Route::resource('categories', 'App\Http\Controllers\Category\CategoryController', ['except' => ['create', 'edit']]);
+
+//Products 
+Route::resource('products', 'App\Http\Controllers\Product\ProductController', ['only' => ['index', 'show']]);
+
+//Seller
+Route::resource('sellers', 'App\Http\Controllers\Seller\SellerController', ['only' => ['index', 'show']]);
+
+//Transaction
+Route::resource('transactions', 'App\Http\Controllers\Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+//User 
+Route::resource('users', 'App\Http\Controllers\User\UserController', ['except' => ['create', 'edit']]);
+
+
+
+
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
