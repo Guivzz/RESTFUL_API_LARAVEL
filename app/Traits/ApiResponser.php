cc06578890b0eae2b8d600d60607933b1c95f93c
Guivzz <guivazoliveira2002@gmail.com>
2024-04-07 19:42:44 -0300

    Caching Results with Laravel Cache System

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -5,6 +5,7 @@ namespace App\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use PHPUnit\TestRunner\TestResult\Collector;
@@ -25,7 +26,7 @@ trait ApiResponser
 
     protected function showAll(Collection $collection, $code = 200)
     {
-        if($collection->isEmpty()) {
+        if ($collection->isEmpty()) {
 
             return $this->successResponse(['data' => $collection], $code);
         }
@@ -37,6 +38,7 @@ trait ApiResponser
         $collection = $this->sortData($collection, $transformer);
         $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
+        $collection = $this->cacheResponse($collection);
 
         return $this->successResponse(['data' => $collection], $code);
     }
@@ -67,7 +69,7 @@ trait ApiResponser
 
     protected function sortData(Collection $collection, $transformer)
     {
-        if (request()-> has('sort_by')) {
+        if (request()->has('sort_by')) {
             $attribute = $transformer::originalAttribute(request()->sort_by);
 
             $collection = $collection->sortBy($attribute);
@@ -90,7 +92,8 @@ trait ApiResponser
             $perPage = (int) request()->per_page;
         }
         $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
-        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [ 'path' => LengthAwarePaginator::resolveCurrentPath(),
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
         ]);
         $paginated->appends(request()->all());
         return $paginated;
@@ -103,4 +106,19 @@ trait ApiResponser
         return $transformation->toArray();
     }
 
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{url} ?{$queryString}";
+
+        return Cache::remember($fullUrl, 30, function () use ($data) {
+            return $data;
+        });
+    }
 }
